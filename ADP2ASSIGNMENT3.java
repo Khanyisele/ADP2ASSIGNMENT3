2d0c454fb3e45906c05cb3624eff7a6ced6a34ce
Khanyisele <81498387+Khanyisele@users.noreply.github.com>
2021-06-09 23:46:38 +0200

    Add files via upload

---
+++ ADP2ASSIGNMENT3.java
@@ -0,0 +1,25 @@
+
+public class Stakeholder implements Serializable{
+    private String stHolderId;
+
+    public Stakeholder() {
+    }
+    
+    public Stakeholder(String stHolderId) {
+        this.stHolderId = stHolderId;
+    }
+    
+    public String getStHolderId() {
+        return stHolderId;
+    }
+
+    public void setStHolderId(String stHolderId) {
+        this.stHolderId = stHolderId;
+    }
+
+    @Override
+    public String toString() {
+       return stHolderId;
+    }
+
+}
